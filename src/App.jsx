dd324e68611378b8fc65c085ec098667d445e5a8
shahinexy <shahinexy@gmail.com>
2024-03-12 12:05:04 +0600

    json file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Bottles from './component/Bottles/Bottles'
 
 function App() {
 
   return (
     <>
       <h2>Buy Bottle</h2>
+      <Bottles></Bottles>
     </>
   )
 }
---
+++ src/component/Bottles/Bottles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bottles = () => {
+    return (
+        <div>
+            <h2>All Bottles</h2>
+        </div>
+    );
+};
+
+export default Bottles;
